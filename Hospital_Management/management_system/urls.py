3e37e0bdc6390a64a54de8e6ecc380f31b267588
Adrian_544 <151731177+githuber-234@users.noreply.github.com>
2025-06-01 14:56:57 +0100

    Update urls.py

--- Hospital_Management/management_system/urls.py
+++ Hospital_Management/management_system/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 from .views import (PatientView, MedicalHistoryView, MedicalReportView, 
                     AiAssistantView, InsightsView, DoctorView, HomeView,
-                    DoctorAppointmentsView)
+                    DoctorAppointmentsView, PatientRecordsView, DoctorNotificationsView,
+                    PatientNotificationsView)
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home-page'),
@@ -10,10 +11,14 @@ urlpatterns = [
     path('doctor/', DoctorView.as_view(), name='doctor-dashboard'),
     path('doctor-appointments/', DoctorAppointmentsView.as_view(), name='doctor-appointments'),
     path('medical-history/', MedicalHistoryView.as_view(), name='medical-history'),
+    path('doctor-notifications/', DoctorNotificationsView.as_view(), name='doctor-notifications'),
+    path('medical-notifications/', PatientNotificationsView.as_view(), name='patient-notifications'),
     path('medical-report/', MedicalReportView.as_view(), name='medical-report'),
     path('Ai-assistant/', AiAssistantView.as_view(), name='ai-assistant'),
     path('insights/', InsightsView.as_view(), name='insights'),
+    path('patient-records/', PatientRecordsView.as_view(), name='patient-records'),
     path('not_authorized/', views.not_authorized, name='not_authorized'),
     path('handle-appointment/<int:appointment_id>/<int:accepted_id>/', views.handle_appointment, name='handle_appointment'),
-
+    path('patient-delete-notification/<int:notification_id>/', views.patient_delete_notification, name='patient_delete_notification'),
+    path('doctor-delete-notification/<int:notification_id>/', views.doctor_delete_notification, name='doctor_delete_notification'),
 ]
