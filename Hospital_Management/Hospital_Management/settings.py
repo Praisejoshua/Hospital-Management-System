6a297c3e49946a8de6772bc6e4b5ed0243009938
Joshua Praise <119796335+Praisejoshua@users.noreply.github.com>
2025-07-25 17:34:01 +0100

    Update settings.py

--- Hospital_Management/Hospital_Management/settings.py
+++ Hospital_Management/Hospital_Management/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = 'django-insecure-9^_e^_n9wu+g&9h+#vxrd8h-z88x-e!vac(%hf*#=m*ezzmmp4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
